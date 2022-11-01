da6c6183eb2fbfcab0f2730b626629092fa5d97a
LcRyp <support@lcryp.com>
2022-11-02 00:03:24 +0200

    Update build_msvc.bat

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,11 +1,11 @@
 @echo off
 set prg=lcryp
 "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild.exe" build_msvc\%prg%.sln -property:Configuration=Release -maxCpuCount -verbosity:minimal
-rd /s /q LcRyp-Wallet
-MD LcRyp-Wallet
-MD LcRyp-Wallet\daemon
-copy "build_msvc\x64\Release\%prg%-qt.exe" "LcRyp-Wallet\%prg%-qt.exe"
-copy "build_msvc\x64\Release\%prg%-cli.exe" "LcRyp-Wallet\daemon\%prg%-cli.exe"
-copy "build_msvc\x64\Release\%prg%-tx.exe" "LcRyp-Wallet\daemon\%prg%-tx.exe"
-copy "build_msvc\x64\Release\%prg%d.exe" "LcRyp-Wallet\daemon\%prg%d.exe"
+rd /s /q LcRyp Core
+MD LcRyp Core
+MD LcRyp Core\daemon
+copy "build_msvc\x64\Release\%prg%-qt.exe" "LcRyp Core\%prg%-qt.exe"
+copy "build_msvc\x64\Release\%prg%-cli.exe" "LcRyp Core\daemon\%prg%-cli.exe"
+copy "build_msvc\x64\Release\%prg%-tx.exe" "LcRyp Core\daemon\%prg%-tx.exe"
+copy "build_msvc\x64\Release\%prg%d.exe" "LcRyp Core\daemon\%prg%d.exe"
 pause
