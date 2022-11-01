726827398bb7cf95d8debe3d3cc272302d0aba94
LcRyp <support@lcryp.com>
2022-11-02 01:45:44 +0200

    Update build_msvc.bat

--- build_msvc.bat
+++ build_msvc.bat
@@ -3,7 +3,7 @@ set prg=lcryp
 "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild.exe" build_msvc\%prg%.sln -property:Configuration=Release -maxCpuCount -verbosity:minimal
 rd /s /q "LcRyp Core"
 MD "LcRyp Core"
-MD "LcRyp Core\daemon"
+MD "LcRyp Core\daemon" 
 copy "build_msvc\x64\Release\%prg%-qt.exe" "LcRyp Core\%prg%-qt.exe"
 copy "build_msvc\x64\Release\%prg%-cli.exe" "LcRyp Core\daemon\%prg%-cli.exe"
 copy "build_msvc\x64\Release\%prg%-tx.exe" "LcRyp Core\daemon\%prg%-tx.exe"
